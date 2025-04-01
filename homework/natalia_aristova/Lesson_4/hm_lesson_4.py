276e31b3bfd0fb98f4622449667f9fdf4ee5d49f
Aristovanatalia87@yandex.ru <Aristovanatalia87@yandex.ru>
2025-04-01 23:35:59 +0300

    Aristova N. Homework for lesson 4

---
+++ homework/natalia_aristova/Lesson_4/hm_lesson_4.py
@@ -0,0 +1,13 @@
+my_dict = {}
+my_dict['tuple'] = (1, 5, 6, 7, 8)
+my_dict['list'] = [1, 2, 3, 4, 5]
+my_dict['dict'] = {1: 2, 2: 3, 3: 4, 4: 5, 5: 6}
+my_dict['set'] = {1, 3, 4, 5, 6}
+print(my_dict['tuple'][-1])
+my_dict['list'].append(10)
+my_dict['list'].pop(1)
+my_dict['dict']['i am a tuple'] = 'it is false'
+del my_dict['dict'][5]
+my_dict['set'].add(101)
+my_dict['set'].discard(3)
+print(my_dict)
