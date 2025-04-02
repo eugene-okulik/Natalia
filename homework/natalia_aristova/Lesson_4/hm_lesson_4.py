83e4c91f0ea5d07868157faa3d1330f531228da2
Aristovanatalia87@yandex.ru <Aristovanatalia87@yandex.ru>
2025-04-02 22:40:35 +0300

    Aristova N. Updated homework for lesson 4: added key with type tuple

--- homework/natalia_aristova/Lesson_4/hm_lesson_4.py
+++ homework/natalia_aristova/Lesson_4/hm_lesson_4.py
@@ -6,7 +6,7 @@ my_dict['set'] = {1, 3, 4, 5, 6}
 print(my_dict['tuple'][-1])
 my_dict['list'].append(10)
 my_dict['list'].pop(1)
-my_dict['dict']['i am a tuple'] = 'it is false'
+my_dict['dict'][('i am a tuple',)] = 'it is false'
 del my_dict['dict'][5]
 my_dict['set'].add(101)
 my_dict['set'].discard(3)
