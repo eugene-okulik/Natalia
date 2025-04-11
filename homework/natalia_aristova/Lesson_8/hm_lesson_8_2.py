32202c1e0f59bed10c0597bc8836d624f96bdda2
Aristovanatalia87@yandex.ru <Aristovanatalia87@yandex.ru>
2025-04-12 00:31:38 +0300

    Aristova N. Homework for lesson 8

--- homework/natalia_aristova/Lesson_8/hm_lesson_8_2.py
+++ homework/natalia_aristova/Lesson_8/hm_lesson_8_2.py
@@ -1,5 +1,6 @@
 import sys
 
+
 def progression(limit=100):
     count = 0
     fibo_n_1 = 1
@@ -12,10 +13,11 @@ def progression(limit=100):
         fibo_n_1 = fibo
         count += 1
 
+
 sys.set_int_max_str_digits(100000)
 count2 = 0
 for number in progression(100000):
-    if count2 == 3:  #это 5-ое число, т.к. вычисление начинается с 3-го числа и до 5-го числа нам надо сделать 3 шага
+    if count2 == 3:  # это 5-ое число, т.к. вычисление начинается с 3-го числа и до 5-го числа нам надо сделать 3 шага
         print(f'Пятое число {number}')
     elif count2 == 198:
         print(f'Двухсотое число {number}')
@@ -24,5 +26,4 @@ for number in progression(100000):
     elif count2 == 99998:
         print(f'Стотысячное число {number}')
         break
-    count2 +=1
-
+    count2 += 1
