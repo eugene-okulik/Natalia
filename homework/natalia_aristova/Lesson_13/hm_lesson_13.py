7bc8c432ad33a73106cd4c7e33a2630e1ffe3a77
Aristovanatalia87@yandex.ru <Aristovanatalia87@yandex.ru>
2025-05-09 21:31:21 +0300

    Aristova N. Homework for lesson_13

--- homework/natalia_aristova/Lesson_13/hm_lesson_13.py
+++ homework/natalia_aristova/Lesson_13/hm_lesson_13.py
@@ -8,11 +8,11 @@ hw13_path = os.path.join(eugene_path, 'hw_13', 'data.txt')
 with open(hw13_path, 'r', encoding='utf-8') as data_file:
     for line in data_file.readlines():
         if 'дату' in line[32:]:
-            print(f"На неделю позже будет  {(datetime.strptime(line[3:29], '%Y-%m-%d %H:%M:%S.%f') +
+            print(f"На неделю позже будет  {(datetime.strptime(line[3:29], '%Y-%m-%d %H:%M:%S.%f') + 
                                              timedelta(days=7))}")
         elif 'день' in line[32:]:
-            print(f"{datetime.strptime(line[3:29], '%Y-%m-%d %H:%M:%S.%f' )}  - "
-                  f" это {datetime.strptime(line[3:29],'%Y-%m-%d %H:%M:%S.%f' ).strftime('%A')}")
+            print(f"{datetime.strptime(line[3:29], '%Y-%m-%d %H:%M:%S.%f' )} - "
+                  f"это {datetime.strptime(line[3:29],'%Y-%m-%d %H:%M:%S.%f' ).strftime('%A')}")
         elif 'дней' in line[32:]:
-            print(f'{(datetime.now() - (datetime.strptime(line[3:29], '%Y-%m-%d %H:%M:%S.%f'))).days} '
-                  f'дней назад была дата {datetime.strptime(line[3:29], '%Y-%m-%d %H:%M:%S.%f')}')
+            print(f"{(datetime.now() - (datetime.strptime(line[3:29], '%Y-%m-%d %H:%M:%S.%f'))).days} "
+                  f"дней назад была дата {datetime.strptime(line[3:29], '%Y-%m-%d %H:%M:%S.%f')}")
