7544e736f0b754301c8a39a68bf144acd0795e95
Aristovanatalia87@yandex.ru <Aristovanatalia87@yandex.ru>
2025-05-09 21:23:25 +0300

    Aristova N. Homework for lesson_13

--- homework/natalia_aristova/Lesson_13/hm_lesson_13.py
+++ homework/natalia_aristova/Lesson_13/hm_lesson_13.py
@@ -1,7 +1,6 @@
 import os
 from datetime import datetime, timedelta
 
-
 homework_path = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 eugene_path = os.path.join(homework_path, 'eugene_okulik')
 hw13_path = os.path.join(eugene_path, 'hw_13', 'data.txt')
@@ -9,8 +8,8 @@ hw13_path = os.path.join(eugene_path, 'hw_13', 'data.txt')
 with open(hw13_path, 'r', encoding='utf-8') as data_file:
     for line in data_file.readlines():
         if 'дату' in line[32:]:
-            print(f'На неделю позже будет  {(datetime.strptime(line[3:29], '%Y-%m-%d %H:%M:%S.%f') +
-                                              timedelta(days=7))}')
+            print(f"На неделю позже будет  {(datetime.strptime(line[3:29], '%Y-%m-%d %H:%M:%S.%f') +
+                                             timedelta(days=7))}")
         elif 'день' in line[32:]:
             print(f"{datetime.strptime(line[3:29], '%Y-%m-%d %H:%M:%S.%f' )}  - "
                   f" это {datetime.strptime(line[3:29],'%Y-%m-%d %H:%M:%S.%f' ).strftime('%A')}")
